6b9f835921fa3afabbcbd1377e7cd88d819c6ab8
Leo Nistor <leo.nistor@gmail.com>
2024-10-29 12:46:05 +0200

    check tailwind, prettier setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1 class="text-3xl font-bold text-white">Hello world!</h1>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.700);
+  }
+</style>
