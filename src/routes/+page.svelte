f7781a300a95aa8f9ffa676b537a5427e9b8f0c5
Leo Nistor <leo.nistor@gmail.com>
2024-10-29 13:50:09 +0200

    enough

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,19 @@
-<h1 class="text-3xl font-bold text-white">Hello world!</h1>
+<script lang="ts">
+  import eye from '$lib/assets/eye.png'
+</script>
+
+<div class="container mx-auto min-h-screen w-full p-8">
+  <div class="flex-col items-center justify-center">
+    <h1 class="text-center text-3xl font-bold text-gray-900">Parsed Wink</h1>
+
+    <p class="text-center text-xl font-light text-gray-700">Mobile apps development.</p>
+
+    <img src={eye} alt="Parsed Wink" class="mx-auto w-64" />
+  </div>
+</div>
 
 <style lang="postcss">
   :global(html) {
-    background-color: theme(colors.gray.700);
+    background-color: #03c9e3;
   }
 </style>
